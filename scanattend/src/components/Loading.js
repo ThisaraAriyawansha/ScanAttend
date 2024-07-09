e05bd79778a8fc926ee279d0175c38e3f7f677ab
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-07-09 18:30:29 +0530

    update

--- scanattend/src/components/Loading.js
+++ scanattend/src/components/Loading.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
-import logo from './image/qr.jpg'; // Adjust the path to your logo
+import logo from './image/logo3.webp'; // Adjust the path to your logo
 import './Loading.css'; // Assuming you have custom styles here
 
 const LoadingScreen = ({ onComplete }) => {
@@ -22,12 +22,12 @@ const LoadingScreen = ({ onComplete }) => {
         }
         return prev + 2; // Increment progress more gradually
       });
-    }, 50); // Adjust the interval for smoother animation
+    }, 20); // Adjust the interval for smoother animation (lower values make it smoother)
   };
 
   return (
     <Container className="loading-container">
-      <img src={logo} alt="System Logo" className="loading-logo" />
+      <img src={logo} alt="System Logo" className="loading-logo" style={{ width: '180px', height: '180px' }} />
       <h1 className="loading-title">ScanAttend</h1>
       <p className="loading-description">Streamlining attendance management with QR code technology.</p>
       <Button onClick={startLoading} className="loading-button">Let's Start</Button>
