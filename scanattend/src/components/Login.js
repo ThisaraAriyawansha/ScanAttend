a81a9061dd4887bfe606885aee40b1932b543c56
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-07-17 20:05:49 +0530

    Add style

--- scanattend/src/components/Login.js
+++ scanattend/src/components/Login.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import './LoginPage.css';
+import loginImage from './image/login.jpg';  
 
-const Login = () => {
+const LoginPage = () => {
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      {/* Add your login form here */}
+    <div className="login-page">
+      <div className="image-section">
+        <img src={loginImage} alt="ScanAttend" />
+      </div>
+      <div className="login-section">
+        <h2>Login to ScanAttend</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit" className="loginbtn">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
